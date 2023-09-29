1f9944a54229ce1d215ad234964bdce445db9e44
MissorAD <missorad@Ubuntu.myguest.virtualbox.org>
2023-09-29 10:44:37 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { useEffect } from 'react';
+import './App.css'
+  
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [state, setState] = useState(null);
+
+  const callBackendAPI = async () => {
+    const response = await fetch('http://localhost:3000/Hello');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body;
+  };
+
+  useEffect(() => {
+    callBackendAPI()
+    .then(res => {
+      setState(res.text)
+      console.log(res.text)
+    })
+    .catch(err => console.log(err));
+  }, [])
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+          <div>
+            World:<span></span>
+            {state}
+          </div>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
